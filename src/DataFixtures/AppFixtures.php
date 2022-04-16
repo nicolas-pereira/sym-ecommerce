02a557413d2977ee6137c21f6639e0f0eeb8fa92
nicolas-pereira <nicolas.pereira.dev@gmail.com>
2022-04-16 05:25:20 +0200

    [ADD] products and fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('en_US');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 50; $i += 1) {
+            $product = new Product();
+            $product->setName($this->faker->words($this->faker->numberBetween(1, 3), true));
+            $product->setDescription($this->faker->paragraph($this->faker->numberBetween(1, 4)));
+            $product->setPrice($this->faker->numberBetween(1));
+            $manager->persist($product);
+        }
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Length(min: 2)]
+    private ?string $name;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $description;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $price;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $imageUrl;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private ?DateTimeImmutable $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(?string $imageUrl): self
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
